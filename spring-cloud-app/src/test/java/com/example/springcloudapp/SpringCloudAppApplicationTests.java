c64cd5781fd12e2326b5526fc243ff3946c582d4
liuwei <867268772@qq.com>
2021-03-08 15:15:09 +0800

    springCloudTest
    Signed-off-by: liuwei <867268772@qq.com>

---
+++ spring-cloud-app/src/test/java/com/example/springcloudapp/SpringCloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
